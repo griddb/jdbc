f0bae7c979efbe6f85e705a8af37ea4202594445
knonomura <katsuhiko.nonomura@griddb.org>
2020-06-05 19:59:15 +0900

    add files for Maven

---
+++ make_source_for_mvn.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+mkdir -p src/main/java
+mkdir -p src/main/resources/com/toshiba/mwcloud/gs/sql/internal
+
+./make_source_for_mvn.pl src src/main/java
+cp src/com/toshiba/mwcloud/gs/sql/internal/SQLDriver.properties src/main/resources/com/toshiba/mwcloud/gs/sql/internal
+cp -r src_contrib/com src/main/java
+
