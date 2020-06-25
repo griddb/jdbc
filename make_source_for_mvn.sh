0170220e68557cb4a8fac3b9412481e5111ec6cb
knonomura <katsuhiko.nonomura@griddb.org>
2020-06-25 10:26:10 +0900

    add a copy of META-INF folder

--- make_source_for_mvn.sh
+++ make_source_for_mvn.sh
@@ -6,4 +6,4 @@ mkdir -p src/main/resources/com/toshiba/mwcloud/gs/sql/internal
 ./make_source_for_mvn.pl src src/main/java
 cp src/com/toshiba/mwcloud/gs/sql/internal/SQLDriver.properties src/main/resources/com/toshiba/mwcloud/gs/sql/internal
 cp -r src_contrib/com src/main/java
-
+cp -r src/META-INF src/main/resources
